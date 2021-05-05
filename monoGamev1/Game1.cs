7b4e2e8ea127dd0781941ac99c13afeb833158ee
Michał Pieczka <michalpieczka99@gmail.com>
2021-05-05 19:01:20 +0200

    Add project files.

---
+++ monoGamev1/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct2D1;
+
+namespace monoGamev1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch;
+
+        private Rakieta gracz;
+        private Texture2D control, rakieta, tlo;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new Microsoft.Xna.Framework.Graphics.SpriteBatch(GraphicsDevice);
+
+            rakieta = Content.Load<Texture2D>("rocket1");
+            control = Content.Load<Texture2D>("control");
+            tlo = Content.Load<Texture2D>("niebo");
+
+            gracz = new Rakieta(rakieta);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            gracz.MoveU();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(tlo, new Rectangle(0,0,480,800), Color.White);
+            spriteBatch.Draw(control, new Vector2(0, 583), Color.White);
+            gracz.Draw(rakieta, spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ monoGamev1/Rakieta.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monoGamev1
+{
+    class Rakieta
+    {
+        private readonly Microsoft.Xna.Framework.Graphics.Texture2D texture;
+        private Vector2 position;
+
+        public Rakieta(Microsoft.Xna.Framework.Graphics.Texture2D texture)
+        {
+            position = new Vector2(210, 480);
+            this.texture = texture;
+        }
+
+        public void Draw(Microsoft.Xna.Framework.Graphics.Texture2D rakieta, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            Rectangle rectGracza = new Rectangle(
+                        (int)GetPosition().X,
+                        (int)GetPosition().Y,
+                        rakieta.Width,
+                        rakieta.Height);
+
+            spriteBatch.Draw(rakieta, rectGracza, Color.White);
+
+
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+
+        public void MoveL()
+        {
+            if (0 <= position.X || position.X <= 400)
+            {
+                position.X -= 5;
+            }
+        }
+        public void MoveR()
+        {
+            if (0 <= position.X || position.X <= 400)
+            {
+                position.X += 5;
+            }
+        }
+        public void MoveU()
+        {
+            if (0 <= position.Y || position.Y <= 477)
+            {
+                position.Y -= 5;
+            }
+            
+        }
+        public void MoveD()
+        {
+            if (0 <= position.Y || position.Y <= 477)
+            {
+                position.Y += 5;
+            }
+        }
+    }
+}
